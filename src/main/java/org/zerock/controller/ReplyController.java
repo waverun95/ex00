9efc36de47973ec7fd5d1508bae15ff5cb9f442d
9634 <9634@coreit.co.kr>
2023-02-08 17:56:26 +0900

    2023 2 8 pm 5:56

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -5,12 +5,12 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
+
+import java.util.List;
 
 @RequestMapping("/replies")
 @RestController
@@ -27,5 +27,13 @@ public class ReplyController {
         log.info("Reply Insert Count = " + insertCount);
 
         return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @GetMapping(value = "/pages/{bno}/{page}",produces = {MediaType.APPLICATION_ATOM_XML_VALUE,MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<List<ReplyVO>> getListResponseEntity(@PathVariable("page") int page, @PathVariable("bno") Long bno){
+        log.info("getList.......");
+        Criteria cri = new Criteria(page,10);
+        log.info(cri);
+
+        return new ResponseEntity<>(service.getList(cri,bno),HttpStatus.OK);
     }
 }
