26abe0a907f59e91754b88ad4275fbffab5a67fc
9634 <9634@coreit.co.kr>
2023-02-14 19:00:28 +0900

    2023 2 14 pm 5:56

---
+++ src/main/java/org/zerock/domain/AuthVO.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class AuthVO {
+    private String userid;
+    private String auth;
+}
